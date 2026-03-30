7463f3935bcab0cb972c95cf0f84155f5db7afad
Manuela Beatriz Tapia Cazas <beatriztapiamath164@gmail.com>
2026-03-30 11:46:18 -0400

    Create Delfin.java

---
+++ PRACTICA03/1_java/a_base/Delfin.java
@@ -0,0 +1,10 @@
+public class Delfin extends Mamifero {
+
+    public void metodo1 () {
+        System.out.println("sopla burbujas");
+    }
+
+    public String toString () {
+        return "delfin";
+    }
+}
