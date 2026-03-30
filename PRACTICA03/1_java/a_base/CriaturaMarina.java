512d64d93dd28364ae5ffef365693b37ac239a31
Manuela Beatriz Tapia Cazas <beatriztapiamath164@gmail.com>
2026-03-30 11:31:10 -0400

    Add CriaturaMarina class with methods and toString

---
+++ PRACTICA03/1_java/a_base/CriaturaMarina.java
@@ -0,0 +1,14 @@
+public class CriaturaMarina {
+
+    public void metodo1 () {
+        System.out.println("criatura 1");
+    }
+
+    public void metodo2 () {
+        System.out.println("criatura 2");
+    }
+
+    public String toString () {
+        return "habita en el oceano";
+    }
+}
