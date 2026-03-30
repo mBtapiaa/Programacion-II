7f580bd86c7341cfe9414879e4777da96ec23327
Manuela Beatriz Tapia Cazas <beatriztapiamath164@gmail.com>
2026-03-30 11:49:50 -0400

    Add Mamifero class extending CriaturaMarina

---
+++ PRACTICA03/1_java/b_c_d_modificado/Mamifero.java
@@ -0,0 +1,10 @@
+public class Mamifero extends CriaturaMarina {
+
+    public Mamifero(String nombre) {
+        super(nombre);
+    }
+
+    public void metodo1 () {
+        System.out.println("sangre caliente");
+    }
+}
