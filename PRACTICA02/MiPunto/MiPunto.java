09f54106ba633c31816aa7c859f940d3d7de5bee
Manuela Beatriz Tapia Cazas <beatriztapiamath164@gmail.com>
2026-03-29 22:56:36 -0400

    Add MiPunto class for point representation

---
+++ PRACTICA02/MiPunto/MiPunto.java
@@ -0,0 +1,26 @@
+public class MiPunto {
+
+private double x;
+private double y;
+
+public MiPunto(){
+this.x=0;
+this.y=0;
+}
+
+public MiPunto(double x,double y){
+this.x=x;
+this.y=y;
+}
+
+public double getX(){ return x; }
+public double getY(){ return y; }
+
+public double distancia(MiPunto p){
+return Math.sqrt(Math.pow(this.x-p.x,2)+Math.pow(this.y-p.y,2));
+}
+
+public double distancia(double x,double y){
+return Math.sqrt(Math.pow(this.x-x,2)+Math.pow(this.y-y,2));
+}
+}
