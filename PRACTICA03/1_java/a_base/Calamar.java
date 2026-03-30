d0e4da0daa7d64eb26514b08dcceee147baf1ca8
Manuela Beatriz Tapia Cazas <beatriztapiamath164@gmail.com>
2026-03-30 11:46:52 -0400

    Create Calamar.java

---
+++ PRACTICA03/1_java/a_base/Calamar.java
@@ -0,0 +1,10 @@
+public class Calamar extends CriaturaMarina {
+
+    public void metodo2 () {
+        System.out.println("tentaculos");
+    }
+
+    public String toString () {
+        return "calamar";
+    }
+}
